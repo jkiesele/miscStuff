3c767abe3c5784ecde34aa30edaafa9edbafb161
jkiesele <>
2011-10-21 12:35:46 +0000

    Energy recalibrated loaders

---
+++ root/IsoStudies/LES_load_EE.C
@@ -0,0 +1,9 @@
+{
+  TFile *f1 = TFile::Open(" dcap://dcache-cms-dcap.desy.de//pnfs/desy.de/cms/tier2/store/user/jkiesele/Trees/corrLE/ee_Full_PFIsoOff.root");
+  TDirectory *d = (TDirectory*) f1->Get("writeNTuple");
+  TTree *t = (TTree*)d->Get("NTuple");
+  t->Process("EE_Analysis.C++");
+
+
+
+}
