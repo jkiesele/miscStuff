cfd0b17b2ec2d6a4ef1616082e759c23364edbec
jkiesele <>
2011-10-21 12:41:40 +0000

    loader for recalibrated lepton energy trees

---
+++ root/LES_load_MuMu.C
@@ -0,0 +1,9 @@
+{
+  TFile *f1 = TFile::Open(" dcap://dcache-cms-dcap.desy.de//pnfs/desy.de/cms/tier2/store/user/jkiesele/Trees/corrLE/MuMu_Full.root");
+  TDirectory *d = (TDirectory*) f1->Get("writeNTuple");
+  TTree *t = (TTree*)d->Get("NTuple");
+  t->Process("MuMu_Analysis.C++");
+
+
+
+}
